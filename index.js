1959449640f074fbe8b38b8241d60e17bbf1f34a
Lorenciano Dramayo <lorencianodramayo@gmail.com>
2020-11-20 07:29:17 +0800

    my initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const fs = require( "fs" );
+const path = require("path"); 
+const { readdirSync } = require('fs');
+const beautify = require('js-beautify').html;
+const test = require('web-resource-inliner');
+
+let normalize =  contents  => {
+    return process.platform === "win32" ? contents.replace( /\r\n/g, "\n" ) : contents;
+}
+
+let readFile =  file =>  {
+    return normalize( fs.readFileSync( file, "utf8" ) );
+}
+
+const getDirectories = source =>
+    readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name)
+
+getDirectories("creatives").forEach((dir) => {
+    if (!fs.existsSync(path.join("output", dir))){
+        fs.mkdirSync(path.join("output", dir))
+    }
+
+    test.html( {
+        fileContent: readFile( path.join('creatives', dir, 'index.html' )),
+        relativeTo: path.join('creatives', dir),
+        scripts: true,
+        links: true,
+    },
+    ( err, result ) => {
+        if(!err){
+                fs.writeFile(path.join('output', dir, 'index.html'), beautify(result.replace(/images/g, ".")), function (err) {
+                    if (err) throw err;
+                    console.log(`${dir} inline file is created successfully.`);
+                }); 
+            }
+        }
+    );
+})
