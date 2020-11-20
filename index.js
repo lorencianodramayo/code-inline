4d10297c8bc5ee1024b76da8c4cc2d224b3c70ee
Lorenciano Dramayo <lorencianodramayo@gmail.com>
2020-11-20 20:06:35 +0800

    added code to clone assets from output

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const fs = require( "fs" );
 const path = require("path"); 
-const { readdirSync } = require('fs');
 const beautify = require('js-beautify').html;
 const test = require('web-resource-inliner');
 
@@ -12,31 +11,71 @@ let readFile =  file =>  {
     return normalize( fs.readFileSync( file, "utf8" ) );
 }
 
+const walkSync = (dir, filelist = []) => {
+    fs.readdirSync(dir).forEach(file => {
+
+    filelist = fs.statSync(path.join(dir, file)).isDirectory()
+        ? walkSync(path.join(dir, file), filelist)
+        : filelist.concat(path.join(dir, file));
+
+    });
+    return filelist;
+}
+
 const getDirectories = source =>
-    readdirSync(source, { withFileTypes: true })
+    fs.readdirSync(source, { withFileTypes: true })
     .filter(dirent => dirent.isDirectory())
     .map(dirent => dirent.name)
 
 getDirectories("creatives").forEach((dir) => {
-    if (!fs.existsSync("output")){
-        fs.mkdirSync("output")
-        if (!fs.existsSync(path.join("output", dir))){
-            fs.mkdirSync(path.join("output", dir))
-        }
-    }
+    (!fs.existsSync("output"))
+        ? fs.mkdirSync("output")
+        : null;
 
+    (!fs.existsSync(path.join("output", dir)))
+        ? fs.mkdirSync(path.join("output", dir))
+        : null;
+
+    (!fs.existsSync('creatives/300x600-Test/assets/image.jpg'))
+        ? fs.copyFileSync('creatives/300x600-Test/assets/image.jpg', 'output/300x600-Test/image.jpg')
+        : null;
+
+    walkSync(path.join("creatives", dir)).forEach((file) => {
+        if(!file.match(/js|css|html/g)){
+            (!fs.existsSync(path.join("output", dir, file.split('/').pop(-1))))
+                ? fs.copyFileSync(file, path.join("output", dir, file.split('/').pop(-1)))
+                : null;
+        }
+    })
+    
     test.html( {
-        fileContent: readFile( path.join('creatives', dir, 'index.html' )),
-        relativeTo: path.join('creatives', dir),
+        fileContent: 
+            readFile(path.join('creatives', dir, 'index.html'))
+                .replace(/src="https|src="http/g, 'data-inline-ignore src="https'),
+        relativeTo: 
+            path.join(
+                'creatives', 
+                dir
+            ),
         scripts: true,
         links: true,
+        images: false
     },
     ( err, result ) => {
         if(!err){
-                fs.writeFile(path.join('output', dir, 'index.html'), beautify(result.replace(/images/g, ".")), function (err) {
-                    if (err) throw err;
-                    console.log(`${dir} inline file is created successfully.`);
-                }); 
+            fs.writeFile(
+                path.join(
+                    'output', 
+                    dir, 
+                    'index.html'
+                ), 
+                beautify(
+                    result.replace(/\.\//g, ".").replace(/assets|images/g , '')), 
+                    function (err) {
+                        if (err) throw err;
+                        console.log(`${dir} inline file is created successfully.`);
+                    }
+                ); 
             }
         }
     );
